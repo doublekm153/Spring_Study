e0319005c9c1b447ac44cd7bf750de0036d61bbd
doublekm153 <doublekm153@gmail.com>
2023-12-05 04:55:08 +0900

    first

---
+++ src/test/java/hello/hello_spring/HelloSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring1ApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
